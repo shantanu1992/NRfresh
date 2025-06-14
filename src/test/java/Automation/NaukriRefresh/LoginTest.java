416ad769905f7ba49cab4570cc3bfbf9aff4f489
shantanu <shanmondal92@gmail.com>
2025-06-14 22:44:12 +0530

    comment updated

--- src/test/java/Automation/NaukriRefresh/LoginTest.java
+++ src/test/java/Automation/NaukriRefresh/LoginTest.java
@@ -22,6 +22,7 @@ public class LoginTest {
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[contains(@placeholder,'Enter your active Email ID')]")));
 		driver.findElement(By.xpath("//input[contains(@placeholder,'Enter your active Email ID')]")).sendKeys("shanmondal92@gmail.com");
 		driver.findElement(By.cssSelector("input[placeholder$='password']")).sendKeys("shan12345@123");
+		//adding a comment
 		driver.findElement(By.cssSelector(".loginButton")).click();
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".view-profile-wrapper a")));//
 		driver.findElement(By.cssSelector(".view-profile-wrapper a")).click();

4aafd181e500348f046e355707436ca9da21fc9f
shantanu <shanmondal92@gmail.com>
2025-06-17 21:54:43 +0530

    updated creds reading logic

--- src/test/java/Automation/NaukriRefresh/LoginTest.java
+++ src/test/java/Automation/NaukriRefresh/LoginTest.java
@@ -2,27 +2,39 @@ package Automation.NaukriRefresh;
 
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginTest {
 
 	@Test
-	public void testLogin() {
-		WebDriver driver = new ChromeDriver();
+	public void testLogin() throws IOException {
+		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.naukri.com/");
 		driver.findElement(By.id("login_Layer")).click();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[contains(@placeholder,'Enter your active Email ID')]")));
-		driver.findElement(By.xpath("//input[contains(@placeholder,'Enter your active Email ID')]")).sendKeys("shanmondal92@gmail.com");
-		driver.findElement(By.cssSelector("input[placeholder$='password']")).sendKeys("shan12345@123");
-		//adding a comment
+		
+		Properties property = new Properties();
+		FileReader reader = new FileReader("src/test/resources/testdata/credentials.properties");
+			property.load(reader);
+			
+			String username = property.getProperty("username");
+			String password = property.getProperty("password");
+		driver.findElement(By.xpath("//input[contains(@placeholder,'Enter your active Email ID')]")).sendKeys(username);
+		driver.findElement(By.cssSelector("input[placeholder$='password']")).sendKeys(password);
 		driver.findElement(By.cssSelector(".loginButton")).click();
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".view-profile-wrapper a")));//
 		driver.findElement(By.cssSelector(".view-profile-wrapper a")).click();
